d580bde0c17610da3c2b4de84630bfc307c6a2a6
Pawar Mukesh <pm7244875@gmail.com>
2022-08-09 08:18:31 +0530

    Assignment2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,11 @@
-const express = require('express');
-const abc = require('../introduction/intro')
-const router = express.Router();
+ router .get('/student',function(req,req))
+ {
+ let student=['mohan','karan','rohit']
+ resizeBy.send(student)
+ }
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    res.send('My second ever api!')
-});
-
-
-router.get('/test-you', function(req, res){
-    res.send('This is the second routes implementation')
-})
-
-router.get('/give-me-students-data',function(req, res){
-
-})
-module.exports = router;
-// adding this comment for no reason
+ router.get('/student-detail/karan/', function (req,req){
+    console.log("rohit")
+    let fetails= "rohit" 
+    res.send(details)
+ })
