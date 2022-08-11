17ac2c3f64b54e3026a177fd19c4cf8f8df9f131
Pawar Mukesh <pm7244875@gmail.com>
2022-08-11 11:54:12 +0530

    module

--- src/routes/route.js
+++ src/routes/route.js
@@ -36,3 +36,70 @@ router.get('/student-details/:name', function(req, res){
 })
 
 module.exports = router;
+
+
+//first problem
+router.get('/get-movies',function(req, res){ 
+    let movies1= ["shole ","Rang de basanti","dil mange more","tiranga"]//api is implementation is used to send response for request
+    res.send(movies1)
+})
+
+
+// second problem
+router.get('/get-movies/:indexNumber',function(req, res){ //student detail api he 
+    
+    let movies=['rang de basanti','The shining','Lord of the rings','batman begins']
+    let index = req.params.indexNumber;
+    console.log(movies[index])
+     res.send(movies[index])
+})
+
+
+//Third solution 
+router.get('/get-moviess/:indexNumber',function(req, res){ //student detail api he 
+    
+    let moviesName=['rang de basanti','The shining','Lord of the rings','batman begins']
+    let index = req.params.indexNumber;
+
+     if(index > moviesName.length){
+        return res.send("use a valid index  ")
+     }else{
+    
+     res.send(moviesName[index])
+     }
+})
+
+//forth solution
+router.get('/get-/films',function(req, res){ //student detail api he    
+
+    let moviesName=[ {"id": 1,"name": "The Shining"}, 
+ {"id": 2,"name": "Incendies"}, 
+ {"id": 3,"name": "Rang de Basanti"},
+  {"id": 4,"name": "Finding Nemo"}]
+    res.send(moviesName)
+})
+
+//fifth solution 
+
+router.get('/gel',function(req, res){ //student detail api he    
+
+    let moviesName=[ {"id": 1,"name": "The Shining"}, 
+ {"id": 2,"name": "Incendies"}, 
+ {"id": 3,"name": "Rang de Basanti"},
+  {"id": 4,"name": "Finding Nemo"}]
+    let index = req.params.indexNumber;
+     if(index > moviesName.length){
+        return res.send("no movie exist with this id ")
+     }else{
+     res.send(moviesName[index])
+     }
+})
+
+
+
+
+
+//wite an api which given the missing number an array of stating from 1...e.g [1,2,3,5,6,7] : 4 is missing
+// get ("/soll", function (req,res){
+    //logic : sum ofnumber is n(n*1)/2
+//})
